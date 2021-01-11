040c613091a8eda739c1295373f0a00bed00c8d1
jielias-1 <74678517+jielias-1@users.noreply.github.com>
2021-01-10 21:11:28 -0600

    Added emergency broadcast script

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,4 @@ output "clb_dns_name" {
   description = "The domain name of the load balancer"
 }
 #Test
+#This is a test of the emergency broadcasting system. This is only a test. If this had been an actual emergency you would have been instructed to tune to one of the broadcast stations in your area.
